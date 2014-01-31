aeb3c929c627ed13094a92934df8568b6ec68fc2
Mohamed <mohamed@mokadev.com>
2014-01-31 15:03:10 +0100

    update cms block source

--- app/code/community/Mokadev/Popin/Model/System/Config/Source/Cms/Block.php
+++ app/code/community/Mokadev/Popin/Model/System/Config/Source/Cms/Block.php
@@ -18,20 +18,11 @@ class Mokadev_Popin_Model_System_Config_Source_Cms_Block
                 ->load();
 
             $res = array();
-            $existingIdentifiers = array();
             foreach ($collection as $item) {
-                $identifier = $item->getData('identifier');
-
-                $data['value'] = $identifier;
-                $data['label'] = $item->getData('title');
-
-                if (in_array($identifier, $existingIdentifiers)) {
-                    $data['value'] .= '|' . $item->getData('page_id');
-                } else {
-                    $existingIdentifiers[] = $identifier;
-                }
-
-                $res[] = $data;
+                $res[] = array(
+                    'value' => $item->getData('block_id'),
+                    'label' => $item->getData('title'),
+                );
             }
             $this->_options = $res;
         }
